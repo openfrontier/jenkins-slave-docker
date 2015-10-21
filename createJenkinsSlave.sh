927f5cfe3560ebb8246bfe6495c54e975630ad3f
zsx <thinkernel@gmail.com>
2015-10-21 19:50:45 +0800

    Change default docker image name

--- createJenkinsSlave.sh
+++ createJenkinsSlave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-JENKINS_SLAVE_IMAGE=${JENKINS_SLAVE_IMAGE:-openfrontier/jenkins-slave:maven}
+JENKINS_SLAVE_IMAGE=${JENKINS_SLAVE_IMAGE:-openfrontier/jenkins-slave}
 JENKINS_VOLUME=${JENKINS_VOLUME:-jenkins-volume}
 PROJECT_NAME=${PROJECT_NAME:-demo}
 NEXUS_REPO=${NEXUS_REPO:-$1}
