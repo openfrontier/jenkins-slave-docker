eaa278169604b6533ea5f7ec5c9beb1212289b34
zsx <thinkernel@gmail.com>
2015-10-28 14:05:56 +0800

    Change default docker image name

--- recreateOAWorkSpace.sh
+++ recreateOAWorkSpace.sh
@@ -3,6 +3,7 @@
 set -e
 
 PROJECT_NAME=oa
+JENKINS_SLAVE_IMAGE=openfrontier/jenkins-slave:sencha
 NEXUS_REPO=http://172.20.200.23/nexus/content/groups/public/
 
 source ~/jenkins-slave-docker/destroyJenkinsSlave.sh
